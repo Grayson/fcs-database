d8d8d4b6c9fd4b0a49ecb78f4c4671fe3ec0e187
Grayson <info@fromconcentratesoftware.com>
2015-10-14 19:40:11 -0400

    Remove unnecessary parameter from `bind` private method of result

--- SimpleDatabaseObjects/result.h
+++ SimpleDatabaseObjects/result.h
@@ -40,55 +40,55 @@ private:
 	result(const statement & statement, Args... args)
 		: m_statement { statement }
 	{
-		bind(*this, 1 /* documented as being position of first parameter */, args...);
+		bind(1 /* documented as being position of first parameter */, args...);
 	}
 	
-	void bind(const result & res, int location) { /* noop */ };
+	void bind(int location) { /* noop */ };
 	
 	template<typename... Args>
-	void bind(const result & res, int location, int value, Args... args)
+	void bind(int location, int value, Args... args)
 	{
-		int status = sqlite3_bind_int(res.m_statement, location, value);
+		int status = sqlite3_bind_int(m_statement, location, value);
 		(void)status; /* TODO: Do something with status */
-		bind(res, location+1, args...);
+		bind(location+1, args...);
 	}
 	
 	template<typename... Args>
-	void bind(const result & res, int location, double value, Args... args)
+	void bind(int location, double value, Args... args)
 	{
-		sqlite3_bind_double(res.m_statement, location+1, value);
-		bind(res, location+1, args...);
+		sqlite3_bind_double(m_statement, location+1, value);
+		bind(location+1, args...);
 	}
 	
 	template<typename... Args>
-	void bind(const result & res, int location, const std::string & string, Args... args)
+	void bind(int location, const std::string & string, Args... args)
 	{
-		sqlite3_bind_text(res.m_statement, location, string.c_str(), static_cast<int>(string.size()), SQLITE_STATIC);
-		bind(res, location+1, args...);
+		sqlite3_bind_text(m_statement, location, string.c_str(), static_cast<int>(string.size()), SQLITE_STATIC);
+		bind(location+1, args...);
 	}
-
+	
 	template<typename TValue, typename ...Args>
-	void bind(const result & res, int _, const std::pair<const char *, TValue> & keyValuePair, Args... args)
+	void bind(int _, const std::pair<const char *, TValue> & keyValuePair, Args... args)
 	{
-		int status = sqlite3_bind_int(res.m_statement, sqlite3_bind_parameter_index(res.m_statement, keyValuePair.first), keyValuePair.second);
+		int status = sqlite3_bind_int(m_statement, sqlite3_bind_parameter_index(m_statement, keyValuePair.first), keyValuePair.second);
 		(void)status; /* TODO: Do something with status */
-		bind(res, std::numeric_limits<int>::min(), args...);
+		bind(std::numeric_limits<int>::min(), args...);
 	}
 	
 	template<typename TValue, typename ...Args>
-	void bind(const result & res, int _, const std::pair<std::string, TValue> & keyValuePair, Args... args)
+	void bind(int _, const std::pair<std::string, TValue> & keyValuePair, Args... args)
 	{
-		bind(res, _, std::make_pair(keyValuePair.first.c_str(), keyValuePair.second), args...);
+		bind(_, std::make_pair(keyValuePair.first.c_str(), keyValuePair.second), args...);
 	}
 	
 	template<typename TValue, typename ...Args>
-	void bind(const result & res, int _, const std::pair<int, TValue> & indexValuePair, Args... args)
+	void bind(int _, const std::pair<int, TValue> & indexValuePair, Args... args)
 	{
-		bind(res, indexValuePair.first, indexValuePair.second, args...);
+		bind(indexValuePair.first, indexValuePair.second, args...);
 	}
 	
 	friend class database;
 };
-	
+
 }
 }

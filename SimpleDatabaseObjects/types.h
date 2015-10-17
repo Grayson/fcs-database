a8849831b25a4642e6ab1ec81355d4b1736bd655
Grayson <info@fromconcentratesoftware.com>
2015-10-17 12:12:20 -0400

    Adding nullable type

--- SimpleDatabaseObjects/types.h
+++ SimpleDatabaseObjects/types.h
@@ -10,5 +10,31 @@ namespace database
 class null_t {};
 static const null_t null {};
 
+template<typename T>
+class nullable
+{
+public:
+	nullable() {};
+	nullable(const T & value)
+		: m_value { value }
+		, m_hasValue { true }
+	{};
+	
+	bool has_value() const { return m_hasValue; }
+	
+	operator const T & () const { return m_value; }
+	operator bool () const { return has_value(); }
+	
+private:
+	T m_value;
+	bool m_hasValue = false;
+};
+
+template<typename T>
+bool operator == (nullable<T> nullableValue, T value)
+{
+	return nullableValue.has_value() && (T)nullableValue == value;
+}
+
 }
 }
